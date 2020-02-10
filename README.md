e5fbdf7a941d503ea7f8ab45d6665ccd4b36310c
ciakkig <ciakki.g@gmail.com>
2020-02-10 23:59:34 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# pycosfic
+# PyCOSFiC: Python Component Optimiser for Simple Filter Circuits
+
+This little script helps automate the picking of components for elementary R-C / R-L filter circuits to get as close as possible to the specified cutoff parameter.
+
+## Dependencies
+
+Just Python. Version 3.6 is required, but for anything other than the f-string formatting style any 3.x version will do too.
+
+
+## Installation
+
+Simply clone this repo or download the files and install the folder wherever you prefer.
+
+
+## Usage
+
+##### COMPONENTS
+The text files in the `components` folder are dedicated to hosting the lists of available components. Do modify them according to your current availability, just make sure to format them as follows:    
+`[value] [magnitude]`, where `value` is the value's figure and `magnitude` is the power of 10 by which multiply the figure:
+- `T`: 12
+- `G`: 9
+- `M`: 6
+- `K`: 3
+- `-`: 1
+- `m`: -3
+- `u`: -6
+- `n`: -9
+- `p`: -12
+Quite like scientific notation.
+
+##### SCRIPT
+Simply fire up `Python cosfic.py` in your terminal and follow the prompts.
+The cutoff parameter is requested formatted as:
+`[f, w] [x]e±[ex]`, where:
+- `f` or `w` is the switch for **frequency** or **pulsation (omega)** as quantities to specify the value for;
+- `x` is the figure for the selected quantity;
+- `ex` is the power of 10 by which multiply the figure, with + or - preposed.
---
+++ cosfic.py
@@ -0,0 +1,109 @@
+## Filter Circuit Optimiser
+# -------------------------
+# by Giorgio Ciacchella (https://github.com/ciakkig)
+# created:		Feb 2020
+# last updated:	Feb 2020
+
+import math
+
+mag_dict = {
+	"T":	12,
+	"G":	9,
+	"M":	6,
+	"K":	3,
+	"-":	0,
+	"m":	-3,
+	"u":	-6,
+	"n":	-9,
+	"p":	-12
+}
+
+fn_dict = {
+	"rc": lambda r, c: 1 / (r*c),
+	"rl": lambda r, l: r*l
+}
+
+res_name = "components/resistors.txt"
+cap_name = "components/capacitors.txt"
+ind_name = "components/inductors.txt"
+
+def loader(res_name, cap_name, ind_name):
+	res_list, cap_list, ind_list = [], [], []
+
+	with open(res_name) as res:
+		for line in res:
+			if len(line) != 0:
+				res_val = lineprocess(line)
+				res_list.append(res_val)
+	
+	with open(cap_name) as cap:
+		for line in cap:
+			if len(line) != 0:
+				cap_val = lineprocess(line)
+				cap_list.append(cap_val)
+	
+	with open(ind_name) as ind:
+		for line in ind:
+			if len(line) != 0:
+				ind_val = lineprocess(line)
+				ind_list.append(ind_val)
+	
+	return res_list, cap_list, ind_list
+
+def lineprocess(line):
+	l = line.strip()
+	f, m = l.split()
+	fig, mag = float(f), m
+	val =  fig * 10**(mag_dict[mag])
+	return val
+
+def inputter():
+	bode = input("input cutoff freq/puls: [f, w] [x]e±[ex] >")
+	switch, bode_n = bode.split()
+	bode_n = float(bode_n)
+	if switch == "f":
+		bode_omega = 2*math.pi*bode_n
+	else:
+		bode_omega = bode_n
+	print(f"{switch} ok.")
+
+	circuit = input("input circuit type: [RC, RL] >")
+	c1 = circuit.lower()
+	comp = c1.strip()
+	fn = fn_dict[comp]
+	print(f"{comp} ok.")
+
+	return bode_omega, fn
+
+def optimiser(list_a, list_b, omega, fn):
+	err_min = float("inf")
+	omega_best = 0
+	comb_best = ()
+	for a in list_a:
+		for b in list_b:
+			omega_temp = fn(a, b)
+			err = math.fabs(omega - omega_temp)
+			if err < err_min:
+				err_min = err
+				omega_best = omega_temp
+				comb_best = (a, b)
+	return omega_best, comb_best
+
+def printer(comb_best, omega_best, bode_omega):
+	a, b = comb_best
+	err = bode_omega - omega_best
+	err_pct = 100 * err / bode_omega
+
+	to_print = f"""best combination:\t{a:.3e}, {b:.3e}
+with omega:\t\t{omega_best:.3e}
+and error:\t\t{err:.3e} ({err_pct:.1f}%) off the input value of {bode_omega:.3e}"""
+
+	print(to_print)
+
+##########
+
+res_list, cap_list, ind_list = loader(res_name, cap_name, ind_name)
+bode_omega, fn = inputter()
+omega_best, comb_best = optimiser(res_list, cap_list, bode_omega, fn)
+
+printer(comb_best, omega_best, bode_omega)
